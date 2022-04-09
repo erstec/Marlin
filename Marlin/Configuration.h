38c4284cfc0f78ab2365cd5b37df7bea7f79ef6b
ErNis <4658853+erstec@users.noreply.github.com>
2022-04-09 20:32:53 +0300

    adjustments

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -935,7 +935,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 800, 1660 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 3200, 555 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -1187,7 +1187,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 37, 8, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 28, 0, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1387,11 +1387,11 @@
 
 // The size of the printable area
 #define X_BED_SIZE 180
-#define Y_BED_SIZE 169
+#define Y_BED_SIZE 180
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS -11
+#define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
